e32206291a4735135e7151e044ca500b0f986b55
Deilons <apolo4748@gmail.com>
2024-09-10 09:27:26 -0500

    Feat: Hotel DTO

--- DTO/Hotel/HotelDTO.cs
+++ DTO/Hotel/HotelDTO.cs
@@ -7,6 +7,12 @@ namespace Dream_Reserve_Back.DTO.Hotel
 {
     public class HotelDTO
     {
-        
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Nit { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Description { get; set; }
     }
 }
